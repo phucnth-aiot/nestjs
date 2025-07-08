cc56fab031e0d53d96fd7560601d23bc1b5cf371
nguyenthanhhoangphuc109 <phucnth@aiot-global.com>
2025-07-08 09:00:50 +0700

    feat: add.

--- src/common/filters/global-exception.filter.ts
+++ src/common/filters/global-exception.filter.ts
@@ -1,5 +1,10 @@
+<<<<<<< HEAD
 import { Catch, ExceptionFilter, ArgumentsHost, HttpStatus, HttpException } from '@nestjs/common';
 import { Request, Response } from 'express';
+=======
+import { Catch, ExceptionFilter, ArgumentsHost, HttpException } from '@nestjs/common';
+import { randomUUID } from 'crypto';
+>>>>>>> e54643e (feat: add.)
 
 @Catch()
 export class GlobalExceptionFilter implements ExceptionFilter {
@@ -7,6 +12,7 @@ export class GlobalExceptionFilter implements ExceptionFilter {
     const ctx = host.switchToHttp();
     const res = ctx.getResponse<Response>();
     const req = ctx.getRequest<Request>();
+<<<<<<< HEAD
     let status = HttpStatus.INTERNAL_SERVER_ERROR;
     let message = 'Internal error';
 
@@ -22,6 +28,16 @@ export class GlobalExceptionFilter implements ExceptionFilter {
       message,
       path: req.url,
       method: req.method,
+=======
+    const status = 500;
+
+    res.status(status).json({
+      statusCode: status,
+      message: exception.message as string,
+      path: req.url,
+      method: req.method,
+      traceId: randomUUID(),
+>>>>>>> e54643e (feat: add.)
     });
   }
 }
